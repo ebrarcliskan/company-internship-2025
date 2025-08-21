669a0b55834a95eaa709205e99657591a73b0624
ebrarcliskan <ebrarcliskan@gmail.com>
2025-08-21 14:02:36 +0300

    feat: personnel-service projesini ekledim

---
+++ personnel-service/src/test/java/com/company/personnelservice/PersonnelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.personnelservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonnelServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
