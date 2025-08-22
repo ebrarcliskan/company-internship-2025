1cd77298eacf32e8e1985c0eb729bc1645332afe
ebrarcliskan <ebrarcliskan@gmail.com>
2025-08-22 14:27:08 +0300

    EmployeeRepository ekledim

---
+++ personnel-service/src/main/java/com/company/personnelservice/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.company.personnelservice.repository;
+
+import com.company.personnelservice.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
